57f8a1b5e570fdeeba8775edca480cb037173c7c
thomasfrisko <friskthom@hotmail.com>
2015-11-20 22:37:54 +0100

    kykles

--- main.cpp
+++ main.cpp
@@ -10,29 +10,19 @@ using std::string;
 int main() {
     cout << "my directory is " << TrainingUtility::PathToExecutable() << "\n";
     cout << "What do you wish to do?\n"
-    <<"Press 1 - Create program\n"
-    <<"Press 2 - Load program\n"
-    <<"Press 3 - Display stats\n"
-    <<"Press 4 - Change stats\n";
+    <<"Press 1 - Display stats\n"
+    <<"Press 2 - Change stats\n";
     int operationCode = 0;
     cin >> operationCode;
 
     switch (operationCode)
     {
         case 1 :
-            break;
-            //call create program function
-        case 2 :
-            break;
-            //call load program function
-        case 3 :
             StatsView::DisplayStats();
             break;
-            //call your stats function
-        case 4 :
+        case 2 :
             StatsView::ChangeStats();
             break;
-            //change stats
         default:
             break;
     }
